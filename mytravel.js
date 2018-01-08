370ed27c16b5e10151d2e7e06b225edd577e351e
Yunfeng <yfdai@buk.cn>
2018-01-08 21:01:20 +0800

    add express and handlebars

--- mytravel.js
+++ mytravel.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
+
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+})
+
+app.get('/', function(req, res) {
+	res.render('home')
+})
+
+app.get('/about', function(req, res){
+	res.render('about')
+})
+
 app.use(function(req, res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 app.use(function(err, req, res, next) {
 	console.error(err.stack)
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.send('500');
 });
 
 app.listen(app.get('port'), function() {
